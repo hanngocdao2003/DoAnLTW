ceb2b50e5f85826fefe7ddbc6bedff7b9bbfefb0
ThanhBinh2219 <21130291@st.hcmuaf.edu.vn>
2023-11-13 21:12:31 +0700

    fix code

---
+++ WebsiteOfBHD/View/event/data.js
@@ -0,0 +1,20 @@
+const listUsers = [
+  {name:"Đào Ngọc Hân", email: "han@gmail.com", phone:"93837483629", address:"Thành phố Hồ Chí Minh", quantityProducts:6},
+    {name: "Nguyễn Văn A", email: "van.a@gmail.com", phone: "912345678", address: "Hà Nội", quantityProducts: 3},
+    {name: "Trần Thị B", email: "thi.b@gmail.com", phone: "988765432", address: "Đà Nẵng", quantityProducts: 8},
+    {name: "Lê Đình C", email: "dinh.c@gmail.com", phone: "977665544", address: "TP. Hồ Chí Minh", quantityProducts: 5},
+    {name: "Phạm Thị D", email: "thi.d@gmail.com", phone: "933221100", address: "Hải Phòng", quantityProducts: 2},
+    {name: "Vũ Văn E", email: "van.e@gmail.com", phone: "965478321", "address": "Cần Thơ", quantityProducts: 7},
+    {name: "Ngọc Hân F", email: "han.f@gmail.com", phone: "938374836", "address": "Thừa Thiên Huế", quantityProducts: 4},
+    {name: "Hoàng Văn G", email: "van.g@gmail.com", phone: "955664422", address: "Quảng Ninh", quantityProducts: 1},
+    {name: "Mai Thị H", email: "thi.h@gmail.com", phone: "911223344", address: "Lào Cai", quantityProducts: 9},
+    {name: "Trần Văn I", email: "van.i@gmail.com", phone: "977889900", address: "An Giang", quantityProducts: 6},
+    {name: "Lê Thị K", email: "thi.k@gmail.com", phone: "933377766", address: "Bình Dương", quantityProducts: 3},
+    {name: "Nguyễn Văn L", email: "van.l@gmail.com", phone: "966655544", address: "Hải Dương", quantityProducts: 8},
+    {name: "Phạm Thị M", email: "thi.m@gmail.com", phone: "933300011", address: "Bắc Ninh", quantityProducts: 5},
+    {name: "Đinh Văn N", email: "van.n@gmail.com", phone: "944466677", address: "Đồng Nai", quantityProducts: 2},
+    {name: "Võ Thị P", email: "thi.p@gmail.com", phone: "977788899", address: "Long An", quantityProducts: 7},
+    {name: "Lý Văn Q", email: "van.q@gmail.com", phone: "966655544", address: "Bình Thuận", quantityProducts: 4},
+    {name: "Nguyễn Thị R", email: "thi.r@gmail.com", phone: "933300011", address: "Nghệ An", quantityProducts: 6}
+]
+
