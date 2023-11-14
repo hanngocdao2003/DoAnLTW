3f2c217c35be4509e961717206c4b98e94ab84b5
hanngocdao2003 <21130345@st.hcmuaf.edu.vn>
2023-11-15 04:13:43 +0700

    fix db

--- Database.sql
+++ Database.sql
@@ -1,16 +1,6 @@
 create database shopquanao;
 use shopquanao;
 
-create table bills (
-    id varchar(10) not null primary key,
-    dateCreated date not null,
-    totalPrice int not null,
-    user varchar(10) not null,
-    statusBill text not null check (statusBill in ('Đã thanh toán','Chưa thanh toán')),
-    foreign key (user)
-        references users (id)
-);
-
 create table roles (
     id varchar(10) not null primary key,
     roleName nvarchar(50) not null
@@ -18,14 +8,11 @@ create table roles (
 
 create table users (
     id varchar(10) not null primary key,
-    accountName varchar(50) not null,
-    accountPass varchar(16) not null,
-    userName nvarchar(50) not null,
-    userPhone varchar(10) not null,
-    userAddress nvarchar(100) not null,
-    userEmail varchar(60) not null,
-    dateCreate date not null,
-<<<<<<< HEAD
+    fullName nvarchar(50) not null,
+    phone varchar(10) not null,
+    email varchar(50) not null,
+    password varchar(10) not null,
+    status smallint not null check(status in (0,1,2)), -- 0 là bị vô hiệu hóa , 1 là đã xác thực, 2 là chưa xác thực
     role varchar(10) not null,
     foreign key (role)
         references roles (id)
@@ -36,9 +23,9 @@ create table categories (
     categoryName nvarchar(50) not null check(categoryName in ('Áo', 'Quần', 'Áo khoác', 'Đầm', 'Váy'))
 );
 
-create table category_detail (
+create table category_details (
     id varchar(10) not null primary key,
-    category  varchar(10) not null,
+    category varchar(10) not null,
     type nvarchar(50) not null,
     foreign key (category)
         references categories (id)
@@ -56,19 +43,21 @@ create table colors (
 
 create table suppliers (
     id varchar(10) not null primary key,
-    nameSupplier nvarchar(50) not null,
-    addressSupplier nvarchar(255) not null,
-    phoneSupplier varchar(10) not null
+    name nvarchar(50) not null,
+    address nvarchar(255) not null,
+    phone varchar(10) not null
 );
 
 create table products (
     id varchar(10) not null primary key,
-    nameProduct nvarchar(50) not null,
-    priceProduct int not null  check(priceProduct >0),
-    categoryProduct varchar(10) not null,
-    status smallint not null check(status = 0 or status =1),
-    foreign key (categoryProduct)
-        references category_detail (id)
+    name nvarchar(50) not null,
+    discountCode varchar(10),
+    price int not null  check(price >0),
+    category varchar(10) not null,
+    foreign key (category)
+        references category_details (id),
+	foreign key (discountCode)
+        references discount_codes (id)
 );
 
 create table product_details (
@@ -79,7 +68,7 @@ create table product_details (
     size varchar(10) not null,
     suppliers varchar(10) not null,
     quantity int not null check(quantity >=0),
-    importDate date not null,
+	status smallint not null check(status = 0 or status =1), -- 0 là bị vô hiệu, 1 là sản phẩm đang được bán
     foreign key (product)
         references products (id),
     foreign key (color)
@@ -90,101 +79,32 @@ create table product_details (
         references suppliers (id)
 );
 
+create table bills (
+    id varchar(10) not null primary key,
+    userName nvarchar(50) not null,
+    userPhone varchar(10) not null,
+    userAddress nvarchar(255) not null,
+    dateCreated date not null,
+    note text,
+    user varchar(10) not null,
+    status text not null check (status in ('Đã thanh toán','Chưa thanh toán')),
+    foreign key (user)
+        references users (id)
+);
+
 create table payment_methods (
     id varchar(10) not null primary key,
-    type nvarchar(50) not null check (type = 'Tiền mặt' or type = 'Chuyển khoản')
+    type nvarchar(50) not null check (type in ("Thanh toán khi nhận hàng","Thanh toán Momo","Thanh toán ZaloPay","Chuyển khoản"))
 );
 
 create table bill_details (
     id varchar(10) not null primary key,
     bill varchar(20) not null,
     product varchar(20) not null,
+    status text not null check (status in ('Đang chuẩn bị hàng','Đang giao hàng','Giao hàng thành công')),
     quantity int not null check (quantity >0),
-    price int not null,
     paymentMethod varchar(10) not null,
     foreign key (bill)
-=======
-    bill varchar(10) not null,
-    role varchar(10) not null,
-    foreign key (role)
-        references roles (id),
-    foreign key (bill)
-        references bills (id)
-);
-
-create table categories (
-    id varchar(10) not null primary key,
-    categoryName nvarchar(50) not null check(categoryName in ('áo', 'Quần', 'Áo khoác', 'Đầm', 'váy'))
-);
-
-create table category_detail (
-    id varchar(10) not null primary key,
-    category  varchar(10) not null,
-    type nvarchar(50) not null,
-    foreign key (category)
-        references categories (id)
-);
-
-create table sizes (
-    id varchar(10) not null primary key,
-    size varchar(5) not null check (size in ('S','M','L'))
-);
-
-create table colors (
-    id varchar(10) not null primary key,
-    color nvarchar(20) not null
-);
-
-create table suppliers (
-    id varchar(10) not null primary key,
-    nameSupplier nvarchar(50) not null,
-    addressSupplier nvarchar(255) not null,
-    phoneSupplier varchar(10) not null
-);
-
-create table products (
-    id varchar(10) not null primary key,
-    nameProduct nvarchar(50) not null,
-    priceProduct int not null  check(priceProduct >0),
-    categoryProduct varchar(10) not null,
-    status smallint not null check(status = 0 or status =1),
-    foreign key (categoryProduct)
-        references category_detail (id)
-);
-
-create table product_details (
-    id varchar(10) not null primary key,
-    product varchar(10) not null,
-    details text not null,
-    color varchar(10) not null,
-    size varchar(10) not null,
-    suppliers varchar(10) not null,
-    quantity int not null check (quantity >= 0),
-    importdate date not null,
-    foreign key (product)
-        references products (id),
-    foreign key (color)
-        references colors (id),
-    foreign key (size)
-        references sizes (id),
-    foreign key (suppliers)
-        references suppliers (id)
-);
-
-create table payment_methods (
-    id varchar(10) not null primary key,
-    type nvarchar(50) not null check (type = 'tiền mặt' or type = 'Chuyển khoản')
-);
-
-create table bill_details (
-    id varchar(10) not null primary key,
-    bill varchar(20) not null,
-    product varchar(20) not null,
-    quantity int not null check (quantity >0),
-    price int not null,
-    paymentMethod varchar(10) not null,
-    foreign key (bill)
->>>>>>> 85a75c510df997d054f78f69698c5249f9d63621
         references bills (id),
     foreign key (product)
         references products (id),
@@ -192,18 +112,38 @@ create table bill_details (
         references payment_methods (id)
 );
 
-<<<<<<< HEAD
 create table images(
 id varchar(10) not null primary key,
 product varchar(10) not null,
-linkImage nvarchar(255) not null,
-=======
-create table image(
-id varchar(10) not null primary key,
-product varchar(10) not null,
-linkimage nvarchar(255) not null,
->>>>>>> 85a75c510df997d054f78f69698c5249f9d63621
+link nvarchar(255) not null,
 foreign key (product)
 references product_details (id)
 );
 
+create table discount_codes(
+	id varchar(10) not null primary key,
+    code varchar(50) not null,
+    percentageOff float not null,
+    dateStart date not null,
+    dateEnd date not null
+);
+
+create table product_reviews(
+id varchar(10) not null primary key,
+user varchar(10) not null,
+bill varchar(10) not null,
+comment text not null,
+stars float not null check (stars between 1 and 5),
+foreign key (bill)
+        references bills (id),
+foreign key (user)
+        references users (id)
+);
+
+create table image_reviews(
+id varchar(10) not null primary key,
+review varchar(10) not null,
+link nvarchar(255) not null,
+foreign key (review)
+	references product_reviews(id)
+);
--- WebsiteOfBHD/View/event/eventAdmin.js
+++ WebsiteOfBHD/View/event/eventAdmin.js
@@ -1,41 +1,41 @@
 const labels = [
-  "Tháng 1",
-  "Tháng 2",
-  "Tháng 3",
-  "Tháng 4",
-  "Tháng 5",
-  "Tháng 6",
+    "Tháng 1",
+    "Tháng 2",
+    "Tháng 3",
+    "Tháng 4",
+    "Tháng 5",
+    "Tháng 6",
 ];
 
 const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: "Sản phẩm đã bán",
-      backgroundColor: "#af6e4e",
-      borderColor: "#af6e4e",
-      data: [0, 20, 40, 25, 58, 72],
-      tension: 0.2,
-    },
-    {
-      label: "Người đăng ký",
-      backgroundColor: "#5883ad",
-      borderColor: "#5883ad",
-      data: [0, 15, 37, 52, 49, 60],
-      tension: 0.2,
-    },
-    {
-      label: "Người truy cập",
-      backgroundColor: "#615c84",
-      borderColor: "#615c84",
-      data: [0, 30, 75, 42, 87, 65],
-      tension: 0.2,
-    },
-  ],
+    labels: labels,
+    datasets: [
+        {
+            label: "Sản phẩm đã bán",
+            backgroundColor: "#af6e4e",
+            borderColor: "#af6e4e",
+            data: [0, 20, 40, 25, 58, 72],
+            tension: 0.2,
+        },
+        {
+            label: "Người đăng ký",
+            backgroundColor: "#5883ad",
+            borderColor: "#5883ad",
+            data: [0, 15, 37, 52, 49, 60],
+            tension: 0.2,
+        },
+        {
+            label: "Người truy cập",
+            backgroundColor: "#615c84",
+            borderColor: "#615c84",
+            data: [0, 30, 75, 42, 87, 65],
+            tension: 0.2,
+        },
+    ],
 };
 const config = {
-  type: "line",
-  data: data,
+    type: "line",
+    data: data,
 };
 const canvas = document.getElementById("canvas");
 const chart = new Chart(canvas, config);
@@ -65,33 +65,33 @@ const chart = new Chart(canvas, config);
 
 // change_template();
 
-// function changeColor(clickedElement) {
-//     var list = document.querySelectorAll('.menu .item');
+function changeColor(clickedElement) {
+    var list = document.querySelectorAll('.menu .item');
 
-//     list.forEach(function (item) {
-//         item.classList.remove('active');
-//     });
-//     clickedElement.classList.add('active');
-// }
+    list.forEach(function (item) {
+        item.classList.remove('active');
+    });
+    clickedElement.classList.add('active');
+}
 
-// function changeDivContent(div) {
-//     var divMain = document.getElementById('info_panel');
-//     var panels = document.querySelectorAll('.info_panel > div');
+function changeDivContent(div) {
+    var divMain = document.getElementById('info_panel');
+    var panels = document.querySelectorAll('.info_panel > div');
 
-//     panels.forEach(function (panel) {
-//         panel.style.display = 'none';
-//     });
+    panels.forEach(function (panel) {
+        panel.style.display = 'none';
+    });
 
-//     var selectedPanel = document.getElementById(div);
-//     if (selectedPanel) {
-//         selectedPanel.style.display = 'block';
-//     }
-// }
+    var selectedPanel = document.getElementById(div);
+    if (selectedPanel) {
+        selectedPanel.style.display = 'block';
+    }
+}
 
-// function handleClick(clickedElement, div) {
-//     changeColor(clickedElement);
-//     changeDivContent(div);
-// }
+function handleClick(clickedElement, div) {
+    changeColor(clickedElement);
+    changeDivContent(div);
+}
 
 // function buttonClick(div) {
 //     let allDivs = document.getElementById('panel4');
