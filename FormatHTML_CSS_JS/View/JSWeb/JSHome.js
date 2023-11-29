66eac0268a26bcec46e1573327a7a33e4087c1a0
HAN NGOC DAO <144197341+hanngocdao2003@users.noreply.github.com>
2023-11-29 14:11:45 +0700

    Delete FormatHTML_CSS_JS directory

--- FormatHTML_CSS_JS/View/JSWeb/JSHome.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("Contact_Us").addEventListener("click", function(){
-        window.scrollTo(0, document.getElementById("footer").offsetTop)
-    });
-})
