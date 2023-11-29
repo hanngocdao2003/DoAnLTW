c5cbf562af5932462a9d9f2fb8b2e68e66b70054
Duong <21130328@st.hcmuaf.edu.vn>
2023-11-29 10:21:50 +0700

    add slick

---
+++ FormatHTML_CSS_JS/View/JSWeb/JSHome.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("Contact_Us").addEventListener("click", function(){
+        window.scrollTo(0, document.getElementById("footer").offsetTop)
+    });
+})
