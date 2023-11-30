15cfda0765f34969eb61a6bafcf1061e461973c4
ThanhBinh2219 <21130291@st.hcmuaf.edu.vn>
2023-11-30 07:37:48 +0700

    update

--- src/main/java/model/dao/ProductData.java
+++ src/main/java/model/dao/ProductData.java
@@ -2,6 +2,7 @@ package Model.dao;
 
 import Model.IProduct;
 import Model.bean.ProductEntity;
+import Model.utils.ConnectionUtils;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -12,31 +13,26 @@ import java.util.List;
 import java.util.Map;
 //
 public class ProductData implements IProduct {
-	static final String url = "jdbc:mysql://localhost:3306/shopquanao";
-	static final String username = "root";
-	static final String password = "trantrantrantran";
-
-//    public String searchWithoutJoin(Map<String, String> search) {
-//        StringBuilder sql = new StringBuilder();
-//        if (search.containsKey("name")) {
-//            String name = search.get("name");
-//            if (name != null && !name.isEmpty()) {
-//                sql.append("AND p.name LIKE '%" + "Áo Len" + "%' ");
-//                /*"Áo len" trong câu trên chỉ là cái để test mẫu,
-//                * Câu đúng nên là sql.append("AND p.name LIKE '%" + name + "%' ");*/
-//            }
-//        }
-//        return sql.toString();
-//    }
+	
+    public String searchWithoutJoin(Map<String, String> search) {
+        StringBuilder sql = new StringBuilder();
+        if (search.containsKey("name")) {
+            String name = search.get("name");
+            if (name != null && !name.isEmpty()) {
+                sql.append("AND p.name LIKE '%" + name + "%' ");
+            }
+        }
+        return sql.toString();
+    }
 
     public String searchWithJoin(Map<String, String> search) {
         StringBuilder sql = new StringBuilder();
-//        if (search.containsKey("category")) {
-//            String category = search.get("category");
-//            if (category != null && !category.isEmpty()) {
-                sql.append("AND c.categoryName LIKE '%Quần%'");
-//            }
-//        }
+        if (search.containsKey("category")) {
+            String category = search.get("category");
+            if (category != null && !category.isEmpty()) {
+                sql.append("AND c.categoryName LIKE '%" + category + "%' ");
+            }
+        }
         return sql.toString();
     }
 
@@ -45,17 +41,17 @@ public class ProductData implements IProduct {
 		List<ProductEntity> productEntities = new ArrayList<>();
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT p.* FROM shopquanao.products p ");
-//        if (search.containsKey("category")) {
+        if (search.containsKey("category")) {
 		sql.append("INNER JOIN shopquanao.category_details cd ON p.categoryId = cd.id\n"
 				+ "INNER JOIN shopquanao.categories c ON cd.categoryId = c.id ");
-//        }
-		// sql.append(searchWithoutJoin(search));
+        }
+		 sql.append(searchWithoutJoin(search));
        
 		sql.append("WHERE 1 = 1 "); // bắt buộc có WHERE 1 = 1
 		sql.append(searchWithJoin(search));
 		sql.append("GROUP BY p.name");
 
-		try (Connection conn = DriverManager.getConnection(url, username, password);
+		try (Connection conn = ConnectionUtils.getConnection();
 				Statement stmt = conn.createStatement();
 				ResultSet rs = stmt.executeQuery(sql.toString())) {
 
