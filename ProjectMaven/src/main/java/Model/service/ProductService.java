97a0a831a27a124807f96abe41e87437bd0320ea
ThanhBinh2219 <21130291@st.hcmuaf.edu.vn>
2023-12-02 19:53:18 +0700

    update

--- ProjectMaven/src/main/java/Model/service/ProductService.java
+++ ProjectMaven/src/main/java/Model/service/ProductService.java
@@ -8,23 +8,5 @@ import java.util.List;
 import java.util.Map;
 
 public class ProductService {
-    public static void main(String[] args) {
-        // Tạo một đối tượng ProductData
-        ProductData productData = new ProductData();
 
-        // Tạo một Map để chứa các thông tin tìm kiếm (nếu cần)
-        Map<String, String> searchParams = new HashMap<>();
-
-        // Gọi phương thức findAll để lấy danh sách sản phẩm
-        List<ProductEntity> productList = productData.findAll(searchParams);
-
-        // In ra thông tin sản phẩm
-        for (ProductEntity product : productList) {
-        	System.out.println("Mã sản phẩm: " + product.getId());
-            System.out.println("Tên sản phẩm: " + product.getName());
-            System.out.println("Đơn giá: " + product.getPrice());
-            System.out.println("Chi tiết sản phẩm: " + product.getDetails());
-            System.out.println("------------------------");
-        }
-    }
 }
