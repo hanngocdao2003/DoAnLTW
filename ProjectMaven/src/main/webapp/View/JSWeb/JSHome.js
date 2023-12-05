ed5714cc41163c7760f9b23d498bbc2d892b2d1c
ThanhBinh2219 <21130291@st.hcmuaf.edu.vn>
2023-12-05 22:02:38 +0700

    update

--- ProjectMaven/src/main/webapp/View/JSWeb/JSHome.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("Contact").addEventListener("click", function(event){
-        event.preventDefault();
-        window.scrollTo(0, document.getElementById("footer").offsetTop)
-    });
-})
